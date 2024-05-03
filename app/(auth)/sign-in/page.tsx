226767036989aedd304af6e19f7efef86fe04e94
Luciano Gonzalez <luchogonzalez080@gmail.com>
2024-05-03 15:27:34 -0300

    First Commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+export default function SignInPage() {
+  return (
+    <div>
+      <h1>Hello Sign In Page</h1>
+    </div>
+  );
+}
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <div>
+      <h1>Hello Sign Up Page</h1>
+    </div>
+  );
+}
